aaf6938bb94f95d48b2e00880c15b84973757fbe
Zac <zacace@ingoglia.com>
2020-01-24 15:26:28 -0700

    EOD 1/24/20

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function mean(){
         console.log("this is num5: " + num5); //debug line for num 5 var
         console.log("this is num6: " + num6); //debug line for num 6 var
     }
-
+    answer = "";
     var sum = num1 + num2 + num3 + num4 + num5 + num6;
 
     console.log("this is num1: " + num1); //debug line for num 1 var
@@ -94,34 +94,158 @@ function largestNumber(){
         console.log("this is num5: " + num5); //debug line for num 5 var
         console.log("this is num6: " + num6); //debug line for num 6 var
     }
+    answer = "";
+    answer = Math.max(num1, num2, num3, num4, num5, num6);
+    console.log("this is answer: " + answer); //debug line for answer var
+    flag = true;
+    output.innerHTML = answer;
 
-    if(num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5 && num1 > num6){
-        output.innerHTML = num1;
-        flag = true;
+    return answer;
+}
+
+function smallestNumber(){
+    if(flag === true){
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        num3 = parseFloat(num3);
+        num4 = parseFloat(num4);
+        num5 = parseFloat(num5);
+        num6 = parseFloat(num6);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
     }
-    else if(num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5 && num2 > num6){
-        output.innerHTML = num2;
-        flag = true;
+    else if(flag === false){
+        num1 = parseFloat(num1.value);
+        num2 = parseFloat(num2.value);
+        num3 = parseFloat(num3.value);
+        num4 = parseFloat(num4.value);
+        num5 = parseFloat(num5.value);
+        num6 = parseFloat(num6.value);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
     }
-    else if(num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5 && num3 > num6){
-        output.innerHTML = num3;
-        flag = true;
+    answer = "";
+    answer = Math.min(num1, num2, num3, num4, num5, num6);
+    console.log("this is answer: " + answer); //debug line for answer var
+    flag = true;
+    output.innerHTML = answer;
+
+    return answer;
+}
+
+function range(){
+    if(flag === true){
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        num3 = parseFloat(num3);
+        num4 = parseFloat(num4);
+        num5 = parseFloat(num5);
+        num6 = parseFloat(num6);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
     }
-    else if(num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5 && num4 > num6){
-        output.innerHTML = num4;
-        flag = true;
+    else if(flag === false){
+        num1 = parseFloat(num1.value);
+        num2 = parseFloat(num2.value);
+        num3 = parseFloat(num3.value);
+        num4 = parseFloat(num4.value);
+        num5 = parseFloat(num5.value);
+        num6 = parseFloat(num6.value);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
     }
-    else if(num5 > num1 && num5 > num2 && num5 > num3 && num5 > num4 && num5 > num6){
-        output.innerHTML = num5;
-        flag = true;
+    answer = "";
+    var minVal = Math.min(num1, num2, num3, num4, num5, num6);
+    console.log("this is minVal: " + minVal); //debug line for minVal var
+    var maxVal = Math.max(num1, num2, num3, num4, num5, num6);
+    console.log("this is maxVal: " + maxVal); //debug line for maxVal var
+
+    answer = maxVal - minVal;
+    console.log("this is answer: " + answer); //debug line for answer var
+    flag = true;
+    output.innerHTML = answer;
+
+    return answer;
+}
+
+function unique(){
+    if(flag === true){
+        num1 = parseFloat(num1);
+        num2 = parseFloat(num2);
+        num3 = parseFloat(num3);
+        num4 = parseFloat(num4);
+        num5 = parseFloat(num5);
+        num6 = parseFloat(num6);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
     }
-    else if(num6 > num1 && num6 > num2 && num6 > num3 && num6 > num4 && num6 > num5){
-        output.innerHTML = num6;
-        flag = true;
+    else if(flag === false){
+        num1 = parseFloat(num1.value);
+        num2 = parseFloat(num2.value);
+        num3 = parseFloat(num3.value);
+        num4 = parseFloat(num4.value);
+        num5 = parseFloat(num5.value);
+        num6 = parseFloat(num6.value);
+        console.log("this is num1: " + num1); //debug line for num 1 var
+        console.log("this is num2: " + num2); //debug line for num 2 var
+        console.log("this is num3: " + num3); //debug line for num 3 var
+        console.log("this is num4: " + num4); //debug line for num 4 var
+        console.log("this is num5: " + num5); //debug line for num 5 var
+        console.log("this is num6: " + num6); //debug line for num 6 var
+    }
+    answer = "";
+    
+    if(num1 !== num2 && num1 !== num3 && num1 !== num4 && num1 !== num5 && num1 !== num6){
+        answer = num1 + ", ";
+    }
+    if(num2 !== num1 && num2 !== num3 && num2 !== num4 && num2 !== num5 && num2 !== num6){
+        answer += num2 + ", ";
+    }
+    if(num3 !== num1 && num3 !== num2 && num3 !== num4 && num3 !== num5 && num3 !== num6){
+        answer += num3 + ", ";
+    }
+    if(num4 !== num1 && num4 !== num2 && num4 !== num3 && num4 !== num5 && num4 !== num6){
+        answer += num4 + ", ";
+    }
+    if(num5 !== num1 && num5 !== num2 && num5 !== num3 && num5 !== num4 && num5 !== num6){
+        answer += num5 + ", ";
+    }
+    if(num6 !== num1 && num6 !== num2 && num6 !== num3 && num6 !== num4 && num6 !== num5){
+        answer += num6 + ", ";
     }
     else{
-        output.innerHTML = "One or more numbers is equal to another number";
-        flag = true;
+        answer = "No Unique Numbers";
     }
-    return true;
+    // var comma = ',';
+    answer = answer.replace(',', "");
+    console.log("this is answer: " + answer); //debug line for answer var
+    flag = true;
+    output.innerHTML = answer;
+
+    let test = "12345,00";
+    test = test.replace(',', ""); 
+    console.log(test);
+
+
+    return answer;
 }
